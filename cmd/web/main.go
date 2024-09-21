c439ad2f6e79972b0f6bb4989f2592601db9f4fb
GolangExpertAcademy <ypastukhov@bk.ru>
2024-09-22 00:05:50 +0300

    add service web with package gin

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,21 @@
-package main 
+package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("Веб-сервер запущен")
+	router := gin.Default()
+
+	router.LoadHTMLGlob("*.html")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{"title": "GoOrderFlow"})
+	})
+
+	router.Run(":8080")
 }
